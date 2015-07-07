5fcad6608a990b8dd80d6d6c9b23f9095ab50e54
Sergio Martin Pueyo <sergiomartinpueyo@Sergios-MacBook-Pro.local>
2015-07-06 23:54:54 -0300

    Initial commit

---
+++ app/src/main/java/ar/com/martineo14/spotifystreamer/SearchArtistFragment.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2015 Sergio Martin Pueyo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ar.com.martineo14.spotifystreamer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import ar.com.martineo14.spotifystreamer.adapters.ArtistListAdapter;
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SearchArtistFragment extends Fragment {
+
+    private String artistNameSearch;
+    private ArtistListAdapter artistAdapter;
+    private ListView listView;
+    private SearchView searchView;
+    List<Artist> artistResult;
+
+    public SearchArtistFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        listView = (ListView) view.findViewById(R.id.list_artist);
+        searchView = (SearchView) view.findViewById(R.id.search_artist);
+
+        if (savedInstanceState != null) {
+            artistNameSearch = savedInstanceState.getString("artist_name");
+            searchView.setQuery(artistNameSearch, false);
+            updateArtistList();
+        }
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                artistNameSearch = query;
+                updateArtistList();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // TODO Auto-generated method stub
+                return false;
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Artist artist = artistAdapter.getItem(position);
+                Intent intent = new Intent(getActivity(), ArtistDetailActivity.class);
+                intent.putExtra("artist_id", artist.id);
+                intent.putExtra("artist_name", artist.name);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void updateArtistList() {
+        SpotifyApi api = new SpotifyApi();
+        SpotifyService spotify = api.getService();
+        spotify.searchArtists(artistNameSearch, new Callback<ArtistsPager>() {
+            @Override
+            public void success(ArtistsPager artistsPager, Response response) {
+                if (artistsPager.artists.items.size() == 0) {
+                    if (artistAdapter != null) {
+                        artistAdapter.clear();
+                    }
+                    Toast.makeText(getActivity(), getString(R.string.msg_no_artist_found),
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    artistResult = artistsPager.artists.items;
+                    artistAdapter = new ArtistListAdapter(getActivity(), artistResult);
+                    listView.setAdapter(artistAdapter);
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                if (artistAdapter != null) {
+                    artistAdapter.clear();
+                }
+                Toast.makeText(getActivity(), getString(R.string.msg_error_default),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString("artist_name", artistNameSearch);
+        super.onSaveInstanceState(outState);
+    }
+}
---
+++ app/src/main/java/ar/com/martineo14/spotifystreamer/adapters/ArtistListAdapter.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2015 Sergio Martin Pueyo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ar.com.martineo14.spotifystreamer.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import ar.com.martineo14.spotifystreamer.R;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.Image;
+
+/**
+ * Created by sergiomartinpueyo on 6/27/15.
+ */
+public class ArtistListAdapter extends ArrayAdapter<Artist> {
+
+    private Context context;
+
+    public ArtistListAdapter(Activity context, List<Artist> artistModels) {
+        super(context, 0, artistModels);
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Artist artistModel = getItem(position);
+        Image image = new Image();
+        View rootView = LayoutInflater.from(getContext()).inflate(R.layout.artist_line_item, parent, false);
+
+        ImageView artist_image = (ImageView) rootView.findViewById(R.id.img_artist_line_item);
+        //artist_image.setImageURI(artistModel.artistImage);
+        List<Image> images = artistModel.images;
+        if (images != null && images.size() > 0) {
+            Picasso.with(context).load(artistModel.images.get(2).url).into(artist_image);
+        }
+        else
+        {
+            //http://www.the-music-shop.com/wp-content/uploads/2015/02/placeholder.png
+            Picasso.with(context).load(R.drawable.placeholder_music).into(artist_image);
+        }
+        //Picasso.with(context).load(image.url).into(artist_image);
+
+        TextView artist_name = (TextView) rootView.findViewById(R.id.name_artist_line_item);
+        artist_name.setText(artistModel.name);
+
+        return rootView;
+    }
+}
